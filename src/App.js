0fd6eca68ae9353f23cec169f0a1efaa47404cd8
placeparks <86215469+placeparks@users.noreply.github.com>
2023-03-06 01:42:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     name: "Hannan Shaukat",
     location: "Punjab, Pakistan",
     tagline: "I'm a Developer",
-    email: "mrac.eth@thereum.email",
+    email: "mirac.eth@ethereum.email",
     availability: "Open for work",
     brand:
       "Welcome to my blockchain website! As a petroleum engineer with a Masters in total quality management and a full stack developer,I am excited to share my expertise in blockchain technology with you. The combination of my technical background and knowledge of quality management provides me with a unique perspective on how blockchain can revolutionize various industries. Through this website, I aim to explore the potential of blockchain, share the latest developments in the field, and offer insights on how this disruptive technology can transform businesses and organizations. ",
